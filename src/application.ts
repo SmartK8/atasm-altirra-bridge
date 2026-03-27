0ae4b9034922d4ff08ad4bb6a14d5027a1e56e0a
SmartK8 <smartk8@gmail.com>
2026-03-27 14:14:17 +0100

    Add explicit MADS and ATasm build commands

--- src/application.ts
+++ src/application.ts
@@ -15,10 +15,6 @@ import { EmulatorRunner } from './emulators/EmulatorRunner';
 
 import { AsmSymbolProvider } from './explorer/asmSymbols';
 
-
-// -------------------------------------------------------------------------------------
-// Operating System
-// -------------------------------------------------------------------------------------
 export const OSPlatform: any = os.platform();
 export const OSArch: any = os.arch();
 export const IsWindows: boolean = (os.platform() === 'win32');
@@ -27,9 +23,6 @@ export const IsMacOS: boolean = (os.platform() === 'darwin');
 export const Is32Bit: boolean = (os.arch() === 'x32');
 export const Is64Bit: boolean = (os.arch() === 'x64');
 
-// -------------------------------------------------------------------------------------
-// Extension
-// -------------------------------------------------------------------------------------
 export const Id = "cerebus.atasm-altirra-bridge";
 export const Path: string = vscode.extensions.getExtension(Id)!.extensionPath;
 export const Name: string = vscode.extensions.getExtension(Id)!.packageJSON.name;
@@ -44,14 +37,8 @@ export const ChangeLogUri: vscode.Uri = vscode.Uri.parse(`https://marketplace.vi
 export const AtasmBuildFilename: string = "atasm-build.json";
 export const SymbolExplorerFilename: string = "asm-symbols.json";
 
-// -------------------------------------------------------------------------------------
-// Channels
-// -------------------------------------------------------------------------------------
 export const CompilerOutputChannel: vscode.OutputChannel = vscode.window.createOutputChannel("Atasm Build");
 
-// -------------------------------------------------------------------------------------
-// Objects
-// -------------------------------------------------------------------------------------
 export const RunAssembler: AssemblerRunnerBase[] = [
 	new AtasmAssemblerRunner(),
 	new MadsAssemblerRunner(),
@@ -62,16 +49,13 @@ export var WorkspaceFolder: string = "";
 
 export var SymbolExplorer: AsmSymbolProvider;
 
-// -------------------------------------------------------------------------------------
-// Configuration
-// -------------------------------------------------------------------------------------
 export interface AtasmConfigurationDefinition {
-	input: string | undefined;				// Which asm file is the root of the project
-	includes: string[] | undefined;			// List of include folders: -Idir1 -Idir2
-	outputFolder: string | undefined;		// Where is the output going?
-	params: string | undefined;				// What parameters are being sent to atasm
-	symbols: string[] | undefined;			// List of symbols to be set via -D parameters
-	withDebug: boolean;						// Generate debug info?
+	input: string | undefined;
+	includes: string[] | undefined;
+	outputFolder: string | undefined;
+	params: string | undefined;
+	symbols: string[] | undefined;
+	withDebug: boolean;
 }
 
 let buildConfig: AtasmConfigurationDefinition | undefined = undefined;
@@ -92,7 +76,6 @@ export async function EnsureBuildConfigIsLoaded(): Promise<boolean> {
 }
 
 export function SetupSymbolExplorer() {
-	// Setup the constant/label/etc symbol explorer window and callbacks
 	const rootPath = (vscode.workspace.workspaceFolders && (vscode.workspace.workspaceFolders.length > 0)) ? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined;
 	const asmSymbolsProvider = new AsmSymbolProvider(rootPath);
 	SymbolExplorer = asmSymbolsProvider;
@@ -100,7 +83,6 @@ export function SetupSymbolExplorer() {
 	vscode.window.registerTreeDataProvider('asmSymbolExplorer', asmSymbolsProvider);
 	vscode.commands.registerCommand('asmSymbolExplorer.refreshEntry', () => asmSymbolsProvider.refresh());
 	vscode.commands.registerCommand('extension.openSourceAtLine', async info => {
-		// Goto the file and line number
 		const uri = vscode.Uri.joinPath(vscode.Uri.file(info.loc), info.file);
 
 		vscode.workspace.openTextDocument(uri).then(doc => {
@@ -113,18 +95,6 @@ export function SetupSymbolExplorer() {
 	});
 }
 
-/**
- * Load the build configuration json file
- * + V1.9.0 - The build file is now optional (depending on the 'atasm-altirra-bridge.application.configuration.forceUseOfBuildConfigFile' configuration option)
- * If the build file is not found and not forced to be there create some default settings.
- * These are the possible options:
- * - look for the build config.
- * - if it is there use it.
- * - if it is missing, check if it needs to be created
- * - if it is missing and not forced to be there then use a basic default config
- * @param retrying True if we are redoing the load
- * @returns true if its all loaded, false if the file is not there or needs configuration
- */
 export async function LoadBuildConfigAsync(retrying: boolean = false): Promise<boolean> {
 	if (!vscode.workspace.workspaceFolders) {
 		if (!retrying) {
@@ -134,34 +104,23 @@ export async function LoadBuildConfigAsync(retrying: boolean = false): Promise<b
 	}
 	const folder = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
 	if (folder) {
-
 		WorkspaceFolder = folder;
-
-		// 1. Try and read the config file
 		let readFileData:string = '{"outputFolder": "out","withDebug": true}';
 
 		let configuration = GetConfiguration();
 		let forceConfigFile = configuration.get<boolean>(`application.configuration.forceUseOfBuildConfigFile`);
 
 		if (forceConfigFile) {
-
-			// Want to use the build config json file.
-			// Make sure its there and valid
 			try {
 				const readFile = util.promisify(fs.readFile);
 				readFileData = await readFile(path.join(folder || "", AtasmBuildFilename), "utf-8");
 			}
 			catch (err) {
-				// There was a problem loading the atasm-build.json file.
 				buildConfig = undefined;
-
-				// Create the default config file
 				if (!await CreateAtasmBuildJsonAsync()) {
-
 					vscode.window.showInformationMessage('Failed to created the default build configuration file!');
 					return false;
 				}
-				// And try to reload it
 				if (!retrying) {
 					return await LoadBuildConfigAsync(true);
 				}
@@ -169,14 +128,12 @@ export async function LoadBuildConfigAsync(retrying: boolean = false): Promise<b
 			}
 		}
 
-		// 2. Try and parse the config file
 		try {
 			buildConfig = JSON.parse(readFileData);
 			return true;
 		}
 		catch (err) {
 			buildConfig = undefined;
-			// There was a problem parsing the config file.
 			vscode.window.showInformationMessage('Unable to parse the config file. JSON syntax error!');
 			return false;
 		}
@@ -188,11 +145,7 @@ export async function LoadBuildConfigAsync(retrying: boolean = false): Promise<b
 	}
 }
 
-// -------------------------------------------------------------------------------------
-// Functions
-// -------------------------------------------------------------------------------------
-export async function BuildGameAsync(fileUri: vscode.Uri): Promise<boolean> {
-
+export async function BuildGameAsync(fileUri: vscode.Uri, assemblerId?: string): Promise<boolean> {
 	if (!await SaveFilesAndContinueAsync(fileUri)) {
 		vscode.window.showErrorMessage("Unable to save files");
 		return false;
@@ -201,36 +154,26 @@ export async function BuildGameAsync(fileUri: vscode.Uri): Promise<boolean> {
 		vscode.window.showErrorMessage("Build configuration not setup");
 	}
 
-	return await SelectAssembler().BuildGameAsync();
+	return await SelectAssembler(assemblerId).BuildGameAsync();
 }
 
-export async function BuildGameAndRunAsync(fileUri: vscode.Uri): Promise<boolean> {
-	if (!await BuildGameAsync(fileUri)) {
+export async function BuildGameAndRunAsync(fileUri: vscode.Uri, assemblerId?: string): Promise<boolean> {
+	if (!await BuildGameAsync(fileUri, assemblerId)) {
 		return false;
 	}
-	// Build went ok, now launch in Altirra
-	await Emulator.RunGameAsync(SelectAssembler().OutputFileName);
-
-	// Result
+	await Emulator.RunGameAsync(SelectAssembler(assemblerId).OutputFileName);
 	return true;
 }
 
-export async function BuildAndDebugAsync(fileUri: vscode.Uri): Promise<boolean> {
-	// VERY NB: breakpoints is an async filled array.  We need to access it
-	// once here let other async processes run and then it will be available later
+export async function BuildAndDebugAsync(fileUri: vscode.Uri, assemblerId?: string): Promise<boolean> {
 	let breakpoints = vscode.debug.breakpoints;
 
-	if (!await BuildGameAsync(fileUri)) {
+	if (!await BuildGameAsync(fileUri, assemblerId)) {
 		return false;
 	}
-	// Build went ok, now launch in Altirra with debug turned on
 
-	await SaveBreakpoints();
-
-	// Build went ok, now launch in Altirra
-	await Emulator.RunDebuggerAsync(SelectAssembler().OutputFileName);
-
-	// Result
+	await SaveBreakpoints(assemblerId);
+	await Emulator.RunDebuggerAsync(SelectAssembler(assemblerId).OutputFileName);
 	return true;
 }
 
@@ -238,21 +181,16 @@ export async function ResetBuildAsync() {
 	SelectAssembler().ResetBuild();
 }
 
-/**
- * Save the breakpoints to the output folder (as used by the assembler)
- * @returns true if the breakpoints were saved away
- */
-export async function SaveBreakpoints(): Promise<boolean> {
-	var folder = SelectAssembler().OutputBreakpoints;
+export async function SaveBreakpoints(assemblerId?: string): Promise<boolean> {
+	const assembler = SelectAssembler(assemblerId);
+	var folder = assembler.OutputBreakpoints;
 	if (!folder || folder.length === 0) {
 		return false;
 	}
 	var breakLines = "";
 	var lastSrcFile = "";
-	var fixedSrcFile = "";			// corrected \ to /
-
+	var fixedSrcFile = "";
 	var breakpointLines = "";
-
 	var atLines: number[] = [];
 	var numFiles = 0;
 	var numBreakpoints = 0;
@@ -267,14 +205,12 @@ export async function SaveBreakpoints(): Promise<boolean> {
 				var file = path.relative(WorkspaceFolder, point.location.uri.fsPath);
 				if (lastSrcFile !== file.toLowerCase()) {
 					++numFiles;
-					// New source file, if there is data from another one sort the line numbers and write away
 					if (atLines.length > 0) {
 						var sorted: number[] = atLines.sort((n1, n2) => n1 - n2);
 						sorted.forEach(x => breakLines += `${x}` + os.EOL);
 					}
 					lastSrcFile = file.toLowerCase();
 					fixedSrcFile = lastSrcFile.replace(/\\/gi, '/');
-					//breakLines += `Source: ${fixedSrcFile}` + os.EOL;
 					breakLines += `Source: ${point.location.uri.fsPath}` + os.EOL;
 					atLines = [];
 				}
@@ -284,7 +220,6 @@ export async function SaveBreakpoints(): Promise<boolean> {
 					breakpointLines += `bx "${point.condition}"` + os.EOL;
 				}
 				else {
-					// breakpointLines += `bp -k -q \`${fixedSrcFile}:${point.location.range.start.line + 1}\`` + os.EOL;
 					breakpointLines += `bp "\`${point.location.uri.fsPath}:${point.location.range.start.line + 1}\`"` + os.EOL;
 				}
 			}
@@ -296,12 +231,9 @@ export async function SaveBreakpoints(): Promise<boolean> {
 	}
 
 	if (breakLines.length > 0) {
-		// Write the new breakpoint locations to file
 		let writeData = Buffer.from(breakLines, 'utf8');
-		await vscode.workspace.fs.writeFile(vscode.Uri.file(SelectAssembler().OutputBreakpoints), writeData);
+		await vscode.workspace.fs.writeFile(vscode.Uri.file(assembler.OutputBreakpoints), writeData);
 
-		// Write the .atdbg debugger script to disk
-		// 1. Prepend some commands
 		var atdbgLines = ".sourcemode on" + os.EOL;
 		atdbgLines += ".echo" + os.EOL;
 		atdbgLines += '.echo "Loading executable..."' + os.EOL;
@@ -311,11 +243,8 @@ export async function SaveBreakpoints(): Promise<boolean> {
 		atdbgLines += breakpointLines;
 		atdbgLines += `.echo ${numBreakpoints} breakpoints from ${numFiles} files have been set` + os.EOL;
 
-		//console.log(`Writing debug commands to file ${(vscode.Uri.file(SelectAssembler().OutputDebugCmds))}`);
-		//console.log(atdbgLines);
-
 		writeData = Buffer.from(atdbgLines, 'utf8');
-		await vscode.workspace.fs.writeFile(vscode.Uri.file(SelectAssembler().OutputDebugCmds), writeData);
+		await vscode.workspace.fs.writeFile(vscode.Uri.file(assembler.OutputDebugCmds), writeData);
 
 		WriteToCompilerTerminal(`${numBreakpoints} breakpoints from ${numFiles} files have been recorded.`);
 	}
@@ -323,25 +252,17 @@ export async function SaveBreakpoints(): Promise<boolean> {
 	return true;
 }
 
-/**
- * Create or open the 'atasm-build.json' configuration file.
- * Returns true if the file was newly created
- */
 export async function CreateAtasmBuildJsonAsync(): Promise<boolean> {
 	if (!vscode.workspace.workspaceFolders) {
 		vscode.window.showInformationMessage('No folder or workspace opened');
 		return false;
 	}
-	// First check if the 'atasm-build.json' file is already there
-	// if it is on disk then just open it for editing
 	const folderUri = vscode.workspace.workspaceFolders?.[0].uri;
 	if (folderUri) {
 		let filename = path.join(folderUri.fsPath, AtasmBuildFilename);
-
 		let uri = vscode.Uri.file(filename);
 
 		try {
-			// Open the existing file
 			await vscode.workspace.fs.stat(uri);
 			vscode.window.showTextDocument(uri, { viewColumn: vscode.ViewColumn.Active });
 		} catch {
@@ -392,11 +313,8 @@ export async function CreateAtasmBuildJsonAsync(): Promise<boolean> {
 			config += os.EOL + "}" + os.EOL;
 
 			const writeData = Buffer.from(config, 'utf8');
-
 			const fileUri = folderUri.with({ path: path.join(folderUri.fsPath, AtasmBuildFilename) });
-
 			await vscode.workspace.fs.writeFile(fileUri, writeData);
-
 			vscode.window.showTextDocument(uri, { viewColumn: vscode.ViewColumn.Active });
 		}
 
@@ -405,11 +323,6 @@ export async function CreateAtasmBuildJsonAsync(): Promise<boolean> {
 	return false;
 }
 
-/**
- * Write a message to the "Compiler" channel
- * @param message Message to be written to the output
- * @param writeToLog if true then message is written to the console
- */
 export function WriteToCompilerTerminal(message: string, writeToLog: boolean = false): void {
 	CompilerOutputChannel.appendLine(message);
 	if (writeToLog) { console.log(`debugger:${message}`); }
@@ -432,36 +345,26 @@ export function GetConfiguration(): vscode.WorkspaceConfiguration {
 }
 
 export async function ShowStartupMessagesAsync(): Promise<void> {
-	// Prepare
 	let configuration = GetConfiguration();
-
-	// Load settings
 	let showNewVersionMessage = configuration.get<boolean>(`application.configuration.showNewVersionMessage`);
 	let latestVersion = configuration.get<string>(`application.configuration.latestVersion`);
 
-	// Process?
 	if (!showNewVersionMessage || latestVersion === Version) { return; }
 
-	// Update latest version
 	configuration.update(`application.configuration.latestVersion`, Version, vscode.ConfigurationTarget.Global);
 
-	// Buttons
 	let latestChanges = "Learn more about the latest changes";
 	let dontShowMeThisMessage = "Don't show me this message again";
 
-	// Show prompt
 	await vscode.window.showInformationMessage(`Welcome to the new version of ${DisplayName}`,
 		latestChanges, dontShowMeThisMessage)
 		.then(selection => {
 			if (selection === undefined) {
-				// Dismissed
 			}
 			else if (selection === latestChanges) {
-				// Show changelog
 				vscode.env.openExternal(ChangeLogUri);
 			}
 			else if (selection = dontShowMeThisMessage) {
-				// Disable
 				configuration.update(`application.configuration.showNewVersionMessage`, false, vscode.ConfigurationTarget.Global);
 			}
 		});
@@ -471,12 +374,8 @@ export function delay(ms: number) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// --------------------------------------------------------
-// Private
-
 export async function SaveFilesAndContinueAsync(fileUri: vscode.Uri | undefined): Promise<boolean> {
 	let configuration = GetConfiguration();
-
 	let result: boolean = true;
 
 	if (configuration.get<boolean>(`editor.saveAllFilesBeforeBuild`)) {
@@ -492,22 +391,17 @@ export async function SaveFilesAndContinueAsync(fileUri: vscode.Uri | undefined)
 	return result;
 }
 
-export async function getAssemblerCommandLine4Task(filename: string | undefined): Promise<string> {
-	return await SelectAssembler().GetAssemblerCommandLine4Task(filename);
+export async function getAssemblerCommandLine4Task(filename: string | undefined, assemblerId?: string): Promise<string> {
+	return await SelectAssembler(assemblerId).GetAssemblerCommandLine4Task(filename);
 }
 
-/**
- * Select which assembler runner to use
- * @returns The assembler runner to be used
- */
-export function SelectAssembler(): AssemblerRunnerBase {
-	// Use Atasm by default
+export function SelectAssembler(whichAssembler?: string): AssemblerRunnerBase {
 	let configuration = GetConfiguration();
-	let command = (configuration.get<string>('assembler.whichAssembler', 'ATasm'));
+	let command = whichAssembler ?? configuration.get<string>('assembler.whichAssembler', 'ATasm');
 
 	var asmIdx = RunAssembler.findIndex(asm => asm.Id === command);
 	if (asmIdx < 0) {
-		asmIdx = 0; // ATasm by default
+		asmIdx = 0;
 	}
-	return RunAssembler[asmIdx];		
+	return RunAssembler[asmIdx];
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,8 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as application from './application';
 import { WelcomePage } from './pages/welcome';
 import './statusbar';
 
-import { AsmSymbolProvider } from './explorer/asmSymbols';
 import { MemoryViewProvider } from './views/MemoryViewProvider';
 
 import * as path from 'path';
@@ -15,27 +12,15 @@ import * as util from 'util';
 let taskProvider: vscode.Disposable | undefined;
 let atasmConfigWatcher: vscode.FileSystemWatcher | undefined;
 let symbolExplorerConfigWatcher: vscode.FileSystemWatcher | undefined;
-var lastChangedDate: Date = new Date();
 let memoryViewProvider: MemoryViewProvider | undefined;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
-	// Pages
 	let welcomePage = new WelcomePage();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-
-	// Welcome
 	const openWelcomePage = vscode.commands.registerCommand('extension.openWelcomePage', () => {
-		//console.log('User activated command "extension.openWelcomePage"');
 		welcomePage.openPage(context);
 	});
 
-	// Build
-	// Note: apparently the fileUri can be supplied via the command line but we are not going to use it
 	const buildGame = vscode.commands.registerCommand('extension.buildGame', async (fileUri: vscode.Uri) => {
 		await application.BuildGameAsync(fileUri);
 	});
@@ -45,28 +30,45 @@ export async function activate(context: vscode.ExtensionContext) {
 	const buildAndDebug = vscode.commands.registerCommand('extension.buildAndDebug', async (fileUri: vscode.Uri) => {
 		await application.BuildAndDebugAsync(fileUri);
 	});
-	// Open configuration file for build and run settings
+
+	const buildGameMads = vscode.commands.registerCommand('extension.buildGameMads', async (fileUri: vscode.Uri) => {
+		await application.BuildGameAsync(fileUri, "Mads");
+	});
+	const buildGameAndRunMads = vscode.commands.registerCommand('extension.buildGameAndRunMads', async (fileUri: vscode.Uri) => {
+		await application.BuildGameAndRunAsync(fileUri, "Mads");
+	});
+	const buildAndDebugMads = vscode.commands.registerCommand('extension.buildAndDebugMads', async (fileUri: vscode.Uri) => {
+		await application.BuildAndDebugAsync(fileUri, "Mads");
+	});
+
+	const buildGameAtasm = vscode.commands.registerCommand('extension.buildGameAtasm', async (fileUri: vscode.Uri) => {
+		await application.BuildGameAsync(fileUri, "ATasm");
+	});
+	const buildGameAndRunAtasm = vscode.commands.registerCommand('extension.buildGameAndRunAtasm', async (fileUri: vscode.Uri) => {
+		await application.BuildGameAndRunAsync(fileUri, "ATasm");
+	});
+	const buildAndDebugAtasm = vscode.commands.registerCommand('extension.buildAndDebugAtasm', async (fileUri: vscode.Uri) => {
+		await application.BuildAndDebugAsync(fileUri, "ATasm");
+	});
+
 	const createAtasmBuildJson = vscode.commands.registerCommand('extension.createAtasmBuildJson', async () => {
 		await application.CreateAtasmBuildJsonAsync();
 	});
-	// Reset the build process
 	const resetBuild = vscode.commands.registerCommand('extension.resetBuild', async () => {
 		await application.ResetBuildAsync();
 	});
-	// Hook up the folder for config file checking
+
 	const folder = vscode.workspace.workspaceFolders?.[0];
 	if (folder) {
 		let buildConfigPromise: Thenable<vscode.Task[]> | undefined = undefined;
 
 		vscode.window.onDidChangeActiveTextEditor(() => application.ClearBuildConfig());
 
-		// Setup checking the atasm-build.json file
 		atasmConfigWatcher = vscode.workspace.createFileSystemWatcher(path.join(folder.uri.fsPath, application.AtasmBuildFilename));
 		atasmConfigWatcher.onDidChange(() => application.ClearBuildConfig());
 		atasmConfigWatcher.onDidCreate(() => application.ClearBuildConfig());
 		atasmConfigWatcher.onDidDelete(() => application.ClearBuildConfig());
 
-		// Setup checking the asm-symbols.json file
 		symbolExplorerConfigWatcher = vscode.workspace.createFileSystemWatcher(path.join(folder.uri.fsPath, application.SymbolExplorerFilename));
 		symbolExplorerConfigWatcher.onDidChange(() => application.SymbolExplorer.refresh());
 		symbolExplorerConfigWatcher.onDidCreate(() => application.SymbolExplorer.refresh());
@@ -83,38 +85,30 @@ export async function activate(context: vscode.ExtensionContext) {
 		});
 	}
 
-	// Subscriptions (register)
 	context.subscriptions.push(openWelcomePage);
 	context.subscriptions.push(buildGame);
 	context.subscriptions.push(buildGameAndRun);
 	context.subscriptions.push(buildAndDebug);
+	context.subscriptions.push(buildGameMads);
+	context.subscriptions.push(buildGameAndRunMads);
+	context.subscriptions.push(buildAndDebugMads);
+	context.subscriptions.push(buildGameAtasm);
+	context.subscriptions.push(buildGameAndRunAtasm);
+	context.subscriptions.push(buildAndDebugAtasm);
 	context.subscriptions.push(resetBuild);
 
 	context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(configChanged));
 
-	// Register intellisense features
-	//await application.RegisterDocumentSymbolProvidersAsync(context);
-	//await application.RegisterDefinitionProvidersAsync(context);
-	//await application.RegisterReferenceProvidersAsync(context);
-	//await application.RegisterCompletionProvidersAsync(context);
-
-	// Create the view to show the memory layout of the code that was assembled
 	memoryViewProvider = new MemoryViewProvider(context.extensionUri);
 	memoryViewProvider.extContext = context;
 	memoryViewProvider.extOutput = application.CompilerOutputChannel;
 	context.subscriptions.push(vscode.window.registerWebviewViewProvider(MemoryViewProvider.viewType, 	memoryViewProvider));
 
-	// Show welcome messages
 	await application.ShowStartupMessagesAsync();
-
-	// Fix Linux/MacOS exec permissions on shipped atasm
 	await application.SelectAssembler().FixExecPermissions();
-
-	// Setup the constant/label/etc symbol explorer window and callbacks
 	application.SetupSymbolExplorer();
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() { }
 
 export function forwardBuildData(data:string)
@@ -131,23 +125,18 @@ interface AssemblerTaskDefinition extends vscode.TaskDefinition {
 
 async function getAssemblerTasks(): Promise<vscode.Task[]> {
 	let tasks: vscode.Task[] = [];
-
 	let buildConfig: application.AtasmConfigurationDefinition | undefined = undefined;
-
 	let editor = vscode.window.activeTextEditor;
 
 	if (editor && editor.document && editor.document.fileName && (editor.document.languageId === "atasm" || editor.document.languageId === "json" )) {
 		let input: string = editor.document.fileName;
-
 		const folder = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
 		if (folder) {
-
 			try {
 				const readFile = util.promisify(fs.readFile);
 				const readFileData = await readFile(path.join(folder || "", application.AtasmBuildFilename), "utf-8");
 				buildConfig = JSON.parse(readFileData);
 
-				// If there in a input file set then register a task to build the input file.
 				if (buildConfig?.input && buildConfig?.input.trim().length > 0) {
 					let commandLine = await application.getAssemblerCommandLine4Task(undefined);
 					if (commandLine.length) {
@@ -156,13 +145,11 @@ async function getAssemblerTasks(): Promise<vscode.Task[]> {
 							new vscode.ShellExecution(commandLine),
 							["$atasm"]);
 						buildTask.group = vscode.TaskGroup.Build;
-
 						tasks.push(buildTask);
 					}
 				}
 			}
 			catch (err) {
-				// There was a problem loading the atasm-build.json file.
 			}
 		}
 
@@ -174,7 +161,6 @@ async function getAssemblerTasks(): Promise<vscode.Task[]> {
 					new vscode.ShellExecution(commandLine),
 					["$atasm"]);
 				buildTask.group = vscode.TaskGroup.Build;
-
 				tasks.push(buildTask);
 			}
 		}
